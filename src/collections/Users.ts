cec2915a61a0f6d6fccc3c5a444ad649fb723540
Andy <andy.tisba+dev@gmail.com>
2025-03-20 16:06:26 +0100

    feat: role / version

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,13 +1,47 @@
 import type { CollectionConfig } from "payload";
 
+export type UserRole = "admin" | "contributor" | "validator";
+
+export interface User {
+  id: string;
+  email: string;
+  role: UserRole;
+}
+
 export const Users: CollectionConfig = {
   slug: "users",
   admin: {
     useAsTitle: "email",
   },
   auth: true,
+  access: {
+    read: ({ req }) => (req.user && "role" in req.user ? req.user.role === "admin" : false),
+    update: ({ req }) => req.user?.id === req.query.id,
+    delete: () => false,
+  },
   fields: [
     // Email added by default
     // Add more fields as needed
+    {
+      name: "role",
+      type: "select",
+      options: [
+        {
+          label: "Administrateur",
+          value: "admin",
+        },
+        {
+          label: "Contributeur",
+          value: "contributor",
+        },
+        {
+          label: "Validateur",
+          value: "validator",
+        },
+      ],
+      required: true,
+      defaultValue: "contributor", // Default role
+      hidden: false, // Ensure the role is visible in API responses
+    },
   ],
 };
